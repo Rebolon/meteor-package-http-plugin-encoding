544dfb57bf7d982a0123ba8680c0ca447c2fdfc7
Benjamin RICHARD <richard.tribes@gmail.com>
2015-01-21 13:59:12 +0000

    add git url in package.js

--- package.js
+++ package.js
@@ -1,28 +1,28 @@
 Package.describe({
-  name: 'http:plugin-encoding',
-  version: '0.0.1',
-  // Brief, one-line summary of the package.
+  name: 'rebolon:http-plugin-encoding',
+  version: '0.0.2',
   summary: 'Add method getWithEncoding to HTTP package',
-  // URL to the Git repository containing the source code for this package.
-  git: '',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
+  git: 'https://github.com/Rebolon/meteor-package-http-plugin-encoding.git',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0');
   
-  api.use(['meteorhacks:npm', 'underscore', 'http']);
+  api.use('underscore');
+  api.use('http');
   
   api.addFiles(['plugin-encoding.js'], ['server']);
   
-  // need to export HTTP (if http has not been required by main app) 
-  api.export('HTTP');
+  api.imply('http');
 });
 
-Npm.depends({"iconv-lite": "0.4.6",
-    "request": "2.51.0"});
+Npm.depends(
+  {
+    "iconv-lite": "0.4.6",
+    "request": "2.51.0"
+  }
+);
 
 /*
 Package.onTest(function(api) {
